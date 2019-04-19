c3d168dddbab0e3e2a9e8f4a8de386a466a4c27b
msiglreith <m.siglreith@gmail.com>
2019-04-19 23:49:06 +0200

    Refactor towards id api

--- examples/basic.rs
+++ examples/basic.rs
@@ -1,21 +1,17 @@
 use stretch::geometry::Size;
-use stretch::node::Node;
 use stretch::style::*;
 
 fn main() {
-    let node = Node::new(
-        Style {
+    let mut stretch = stretch::node::Stretch::new();
+    let node1 = stretch.create_node(Style { size: Size { width: Dimension::Percent(0.5), height: Dimension::Auto }, ..Default::default() }, vec![]);
+    let node0 = stretch.create_node(Style {
             size: Size { width: Dimension::Points(100.0), height: Dimension::Points(100.0) },
             justify_content: JustifyContent::Center,
             ..Default::default()
-        },
-        vec![&Node::new(
-            Style { size: Size { width: Dimension::Percent(0.5), height: Dimension::Auto }, ..Default::default() },
-            vec![],
-        )],
-    );
-
-    let layout = node.compute_layout(Size::undefined()).unwrap();
+    }, vec![node1]);
+    let layout = stretch.compute_layout(node0, Size::undefined()).unwrap();
 
     println!("{:#?}", layout);
+    println!("{:#?}", stretch.layout(node0));
+    println!("{:#?}", stretch.layout(node1));
 }
--- src/node.rs
+++ src/node.rs
@@ -1,12 +1,8 @@
 #[cfg(not(feature = "std"))]
 use alloc::{vec, vec::Vec};
 
-use std::cell::RefCell;
-use std::fmt;
-use std::rc::Rc;
-use std::rc::Weak;
+use std::collections::HashMap;
 
-use crate::algo;
 use crate::geometry::Size;
 use crate::number::Number;
 use crate::result::{Cache, Layout, Result};
@@ -14,151 +10,221 @@ use crate::style::*;
 
 type MeasureFunc = Box<Fn(Size<Number>) -> Result<Size<f32>>>;
 
-pub(crate) struct InternalNode {
-    pub(crate) style: Style,
-    pub(crate) parents: Vec<Weak<RefCell<InternalNode>>>,
-    pub(crate) children: Vec<Rc<RefCell<InternalNode>>>,
-    pub(crate) measure: Option<MeasureFunc>,
-    pub(crate) layout_cache: Option<Cache>,
-    pub(crate) is_dirty: bool,
+pub type NodeId = usize;
+
+pub(crate) type Storage<T> = HashMap<NodeId, T>;
+
+struct Allocator {
+    new_id: NodeId,
+    free_ids: Vec<NodeId>,
 }
 
-pub struct Node(Rc<RefCell<InternalNode>>);
-
-impl Node {
-    pub fn new_leaf(style: Style, measure: Option<MeasureFunc>) -> Node {
-        Node(Rc::new(RefCell::new(InternalNode {
-            style,
-            parents: Vec::with_capacity(1),
-            children: Vec::with_capacity(0),
-            measure,
-            layout_cache: None,
-            is_dirty: true,
-        })))
-    }
-
-    pub fn new(style: Style, children: Vec<&Node>) -> Node {
-        let parent = Node(Rc::new(RefCell::new(InternalNode {
-            style,
-            parents: Vec::with_capacity(1),
-            children: Vec::with_capacity(children.len()),
-            measure: None,
-            layout_cache: None,
-            is_dirty: true,
-        })));
-
-        for child in children {
-            child.0.borrow_mut().parents.push(Rc::downgrade(&parent.0));
-            parent.0.borrow_mut().children.push(Rc::clone(&child.0));
+impl Allocator {
+    pub fn new() -> Self {
+        Allocator {
+            new_id: 0,
+            free_ids: Vec::new(),
         }
-
-        parent
     }
 
-    pub fn add_child(&mut self, child: &Node) {
-        child.0.borrow_mut().parents.push(Rc::downgrade(&self.0));
-        self.0.borrow_mut().children.push(Rc::clone(&child.0));
-        self.mark_dirty();
-    }
-
-    pub fn set_children(&mut self, children: Vec<&Node>) {
-        for child in &self.0.borrow().children {
-            let position =
-                child.borrow().parents.iter().position(|x| Rc::ptr_eq(&x.upgrade().unwrap(), &self.0)).unwrap();
-            child.borrow_mut().parents.remove(position);
-        }
-
-        self.0.borrow_mut().children = Vec::with_capacity(children.len());
-
-        for child in children {
-            child.0.borrow_mut().parents.push(Rc::downgrade(&self.0));
-            self.0.borrow_mut().children.push(Rc::clone(&child.0));
-        }
-
-        self.mark_dirty();
-    }
-
-    pub fn remove_child(&mut self, child: &Node) -> Node {
-        self.remove_child_at_index({
-            let parent = self.0.borrow();
-            parent.children.iter().position(|x| Rc::ptr_eq(&x, &child.0)).unwrap()
-        })
-    }
-
-    pub fn remove_child_at_index(&mut self, index: usize) -> Node {
-        let child = {
-            let mut parent = self.0.borrow_mut();
-            let child = parent.children.remove(index);
-            let position =
-                child.borrow().parents.iter().position(|x| Rc::ptr_eq(&x.upgrade().unwrap(), &self.0)).unwrap();
-            child.borrow_mut().parents.remove(position);
-            child
-        };
-
-        self.mark_dirty();
-        Node(child)
-    }
-
-    pub fn replace_child_at_index(&mut self, index: usize, child: &Node) -> Node {
-        child.0.borrow_mut().parents.push(Rc::downgrade(&self.0));
-        let old_child = std::mem::replace(&mut self.0.borrow_mut().children[index], Rc::clone(&child.0));
-
-        let position =
-            old_child.borrow().parents.iter().position(|x| Rc::ptr_eq(&x.upgrade().unwrap(), &self.0)).unwrap();
-        old_child.borrow_mut().parents.remove(position);
-
-        self.mark_dirty();
-
-        Node(old_child)
-    }
-
-    pub fn children(&self) -> Vec<Node> {
-        let node = self.0.borrow_mut();
-        node.children.iter().map(|child| Node(Rc::clone(child))).collect()
-    }
-
-    pub fn set_style(&mut self, style: Style) {
-        self.0.borrow_mut().style = style;
-        self.mark_dirty();
-    }
-
-    pub fn style(&self) -> Style {
-        self.0.borrow().style
-    }
-
-    pub fn mark_dirty(&mut self) {
-        let mut node = self.0.borrow_mut();
-        node.layout_cache = None;
-        node.is_dirty = true;
-
-        for parent in &node.parents {
-            if let Some(parent) = parent.upgrade() {
-                Node(parent).mark_dirty();
+    pub fn allocate(&mut self) -> NodeId {
+        match self.free_ids.pop() {
+            Some(id) => id,
+            None => {
+                let id = self.new_id;
+                self.new_id += 1;
+                id
             }
         }
     }
-
-    pub fn dirty(&self) -> bool {
-        self.0.borrow().is_dirty
-    }
-
-    pub fn compute_layout(&self, size: Size<Number>) -> Result<Layout> {
-        algo::compute(&mut self.0.borrow_mut(), size)
-    }
 }
 
-impl fmt::Debug for Node {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let style = &self.0.borrow().style;
-        let children: Vec<_> = self.0.borrow().children.iter().map(|c| Node(Rc::clone(c))).collect();
-        write!(f, "Node {{ style: {:?}, children: {:?} }}", style, children)
-    }
+pub struct Stretch {
+    nodes: Allocator,
+    pub(crate) style: Storage<Style>,
+    pub(crate) parents: Storage<Vec<NodeId>>,
+    pub(crate) children: Storage<Vec<NodeId>>,
+    pub(crate) measure: Storage<Option<MeasureFunc>>,
+    pub(crate) layout: Storage<Layout>,
+    pub(crate) layout_cache: Storage<Option<Cache>>,
+    pub(crate) is_dirty: Storage<bool>,
 }
 
-impl PartialEq for Node {
-    fn eq(&self, other: &Node) -> bool {
-        Rc::ptr_eq(&self.0, &other.0)
+impl Stretch {
+    pub fn new() -> Self {
+        Stretch {
+            nodes: Allocator::new(),
+            style: Storage::new(),
+            parents: Storage::new(),
+            children: Storage::new(),
+            measure: Storage::new(),
+            layout: Storage::new(),
+            layout_cache: Storage::new(),
+            is_dirty: Storage::new(),
+        }
+    }
+
+    pub fn create_leaf(&mut self, style: Style, measure: Option<MeasureFunc>) -> NodeId {
+        let node = self.nodes.allocate();
+        self.style.insert(node, style);
+        self.parents.insert(node, Vec::with_capacity(1));
+        self.children.insert(node, Vec::with_capacity(0));
+        self.measure.insert(node, measure);
+        self.layout.insert(node, Layout::new());
+        self.layout_cache.insert(node, None);
+        self.is_dirty.insert(node, true);
+        node
+    }
+
+    pub fn create_node(&mut self, style: Style, children: Vec<NodeId>) -> NodeId {
+        let node = self.nodes.allocate();
+
+        for child in &children {
+            self.parents.get_mut(&child).unwrap().push(node);
+        }
+
+        self.style.insert(node, style);
+        self.parents.insert(node, Vec::with_capacity(1));
+        self.children.insert(node, children);
+        self.measure.insert(node, None);
+        self.layout.insert(node, Layout::new());
+        self.layout_cache.insert(node, None);
+        self.is_dirty.insert(node, true);
+
+        node
+    }
+
+    pub fn add_child(&mut self, node: NodeId, child: NodeId) {
+        self.children.get_mut(&node).unwrap().push(child);
+        self.parents.get_mut(&node).unwrap().push(node);
+        *self.is_dirty.get_mut(&node).unwrap() = true;
+    }
+
+    pub fn set_children(&mut self, node: NodeId, children: Vec<&NodeId>) {
+        // for child in &self.0.borrow().children {
+        //     let position = child
+        //         .borrow()
+        //         .parents
+        //         .iter()
+        //         .position(|x| Rc::ptr_eq(&x.upgrade().unwrap(), &self.0))
+        //         .unwrap();
+        //     child.borrow_mut().parents.remove(position);
+        // }
+
+        // self.0.borrow_mut().children = Vec::with_capacity(children.len());
+
+        // for child in children {
+        //     child.0.borrow_mut().parents.push(Rc::downgrade(&self.0));
+        //     self.0.borrow_mut().children.push(Rc::clone(&child.0));
+        // }
+
+        // self.mark_dirty();
+
+        unimplemented!()
+    }
+
+    pub fn remove_child(&mut self, child: NodeId) {
+        // self.remove_child_at_index({
+        //     let parent = self.0.borrow();
+        //     parent
+        //         .children
+        //         .iter()
+        //         .position(|x| Rc::ptr_eq(&x, &child.0))
+        //         .unwrap()
+        // })
+
+        unimplemented!()
+    }
+
+    pub fn remove_child_at_index(&mut self, index: usize) {
+        // let child = {
+        //     let mut parent = self.0.borrow_mut();
+        //     let child = parent.children.remove(index);
+        //     let position = child
+        //         .borrow()
+        //         .parents
+        //         .iter()
+        //         .position(|x| Rc::ptr_eq(&x.upgrade().unwrap(), &self.0))
+        //         .unwrap();
+        //     child.borrow_mut().parents.remove(position);
+        //     child
+        // };
+
+        // self.mark_dirty();
+        // Node(child)
+
+        unimplemented!()
+    }
+
+    pub fn replace_child_at_index(&mut self, index: usize, child: NodeId) {
+        // child.0.borrow_mut().parents.push(Rc::downgrade(&self.0));
+        // let old_child = std::mem::replace(
+        //     &mut self.0.borrow_mut().children[index],
+        //     Rc::clone(&child.0),
+        // );
+
+        // let position = old_child
+        //     .borrow()
+        //     .parents
+        //     .iter()
+        //     .position(|x| Rc::ptr_eq(&x.upgrade().unwrap(), &self.0))
+        //     .unwrap();
+        // old_child.borrow_mut().parents.remove(position);
+
+        // self.mark_dirty();
+
+        // Node(old_child)
+
+        unimplemented!()
+    }
+
+    pub fn children(&self, node: NodeId) -> Vec<NodeId> {
+        self.children[&node].clone()
+    }
+
+    pub fn set_style(&mut self, node: NodeId, style: Style) {
+        if let Some(s) = self.style.get_mut(&node) {
+            *s = style;
+            *self.is_dirty.get_mut(&node).unwrap() = true;
+        }
+    }
+
+    pub fn style(&self, node: NodeId) -> Style {
+        self.style[&node]
+    }
+
+    pub fn layout(&self, node: NodeId) -> Layout {
+        self.layout[&node]
+    }
+
+    pub fn mark_dirty(&mut self, node: NodeId) {
+        fn mark_dirty_impl(
+            node: NodeId,
+            layout_cache: &mut Storage<Option<Cache>>,
+            is_dirty: &mut Storage<bool>,
+            parents: &Storage<Vec<NodeId>>,
+        ) {
+            *layout_cache.get_mut(&node).unwrap() = None;
+            *is_dirty.get_mut(&node).unwrap() = true;
+
+            for parent in &parents[&node] {
+                mark_dirty_impl(*parent, layout_cache, is_dirty, parents);
+            }
+        }
+
+        mark_dirty_impl(
+            node,
+            &mut self.layout_cache,
+            &mut self.is_dirty,
+            &self.parents,
+        );
+    }
+
+    pub fn dirty(&self, node: NodeId) -> bool {
+        self.is_dirty[&node]
+    }
+
+    pub fn compute_layout(&mut self, node: NodeId, size: Size<Number>) -> Result<()> {
+        self.compute(node, size)
     }
 }
-
-impl Eq for Node {}
